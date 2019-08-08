510b1f9d69ea5c0448b4859a02aba43789e47784
revoidea <eddiedi@126.com>
2019-08-08 20:44:16 +0800

    新增分类功能

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>新建分类</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+       <el-form-item label="名称">
+         <el-input v-model="model.name"> </el-input>
+       </el-form-item>
+       <el-form-item>
+         <el-button type="primary" native-type="submit">保存</el-button>
+       </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        model:{}
+      }
+    },
+    methods:{
+      async save(){
+        
+        //const result = await this.$http.post('categories',this.model)
+        await this.$http.post('categories',this.model)
+        this.$router.push('/categories/list')
+        //element-ui 提供的方法
+        this.$message({
+          type:'success',
+          message:'保存成功'
+        })
+      }
+    }
+  };
+</script>
---
+++ server/routes/admin/index.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+   const express = require('express')
+   const router = express.Router()
+   const Category = require('../../modules/Category')
+    
+   router.post('/categories',async(req,res) =>{
+       const model = await  Category.create(req.body)
+       res.send(model)
+   });
+
+
+   router.get('/categories',async(req,res) =>{
+       const items = await Category.find().limit(10)
+       res.send(items);
+   })
+   app.use('/admin/api',router);
+}
